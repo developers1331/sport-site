b65fcc71e3aa6a5b2675e5c250929d955e81671a
crone <pskovstudent@mail.ru>
2022-04-05 23:32:10 +0300

    chore: add preloader

--- src/app/modules/posts/posts.module.ts
+++ src/app/modules/posts/posts.module.ts
@@ -6,10 +6,11 @@ import { RouterModule } from '@angular/router';
 import { PostsService } from 'src/app/general/services/posts.service';
 import { PostPageComponent } from './post-page/post-page.component';
 import { QuillModule } from 'ngx-quill';
+import { SharedModuleModule } from 'src/app/modules/shared-module/shared-module.module';
 
 @NgModule({
     declarations: [PostsWrapperComponent, PostComponent, PostPageComponent],
-    imports: [CommonModule, RouterModule, QuillModule],
+    imports: [CommonModule, RouterModule, QuillModule, SharedModuleModule],
     exports: [PostsWrapperComponent, PostComponent],
     providers: [PostsService],
 })
