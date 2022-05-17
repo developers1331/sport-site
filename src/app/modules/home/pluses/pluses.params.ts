aa088f71271bee8f0f99c5363c3a9ee15fbcde99
Doken1331 <lass245@mail.ru>
2022-05-18 02:41:17 +0300

    chore: $06 - added bodybuilding icons

--- src/app/modules/home/pluses/pluses.params.ts
+++ src/app/modules/home/pluses/pluses.params.ts
@@ -13,24 +13,28 @@ export const dataPluses: Ipluses[] = [
         id: 0,
         elems: [
             {
-                img: 'assets/images/run-icon.svg',
-                title: 'Training',
-                description: 'Крута. Lorem ipsum, dolor sit amet consectetur',
+                img: 'assets/images/icons/body-icon-1.png',
+                title: 'Красивое тело',
+                description:
+                    'Под влиянием систематических силовых нагрузок происходит утолщение мышечных волокон, а также увеличение их количества. Хорошо развитая мускулатура — фундамент здоровья, ибо мышцы не просто существуют с другими органами, они активно на них воздействуют, помогают им в работе.',
             },
             {
-                img: 'assets/images/run-icon.svg',
-                title: 'Training',
-                description: 'Крута. Lorem ipsum, dolor sit amet consectetur',
+                img: 'assets/images/icons/body-icon-2.png',
+                title: 'Иммунитет',
+                description:
+                    'Занятия спортом позволяют иммунной системе «разогнаться» в прямом смысле этого слова. Во время физических нагрузок увеличивается скорость циркулирующей крови. Следовательно, иммунные клетки быстрее попадают во все ткани и органы.',
             },
             {
-                img: 'assets/images/run-icon.svg',
-                title: 'Training',
-                description: 'Крута. Lorem ipsum, dolor sit amet consectetur',
+                img: 'assets/images/icons/body-icon-3.png',
+                title: 'Повышение силы',
+                description:
+                    'Бодибилдинг не только из-за рост мышц, но благодаря лучшему биомеханическому и нервно-мышечному контролю он позволяет улучшить силу, увеличить ее и является весьма удобным даже для людей, нуждающихся в этом преимуществе, таких как пожилые люди, люди с мышечным дисбалансом, люди с потерей силы из-за некоторых типов возбудителя.',
             },
             {
-                img: 'assets/images/run-icon.svg',
-                title: 'Training',
-                description: 'Крута. Lorem ipsum, dolor sit amet consectetur',
+                img: 'assets/images/icons/body-icon-4.png',
+                title: 'Сердечно-сосудистая система',
+                description:
+                    'Бодибилдинг тренирует сердечную мышцу. Сила сердца и его объем резко возрастают. Сердце приобретает способность выбрасывать в минуту до 42 литров крови! Стенки сосудов становятся эластичными. Оживают мелкие периферийные капилляры, постепенно угасающие с возрастом.',
             },
         ],
     },
